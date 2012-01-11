097dd108a3ddc42edca1c3688db591df167c99b0
Jonathan Clem <jotclem+git@gmail.com>
2012-01-10 22:18:44 -0500

    zsh has its own alias file.

--- zsh-custom/aliases.zsh
+++ zsh-custom/aliases.zsh
@@ -1,1 +1,50 @@
-../bash/aliases
+# Directory traversal
+alias ..="cd .."
+alias ...="cd ../.."
+alias -- -="cd -"
+
+# Prettier, human-readable file lising.
+alias l="ls -aFGhl"
+
+# Various shortcuts
+alias db="~/Dropbox"
+alias v="vim"
+alias m="mvim"
+
+# Show/hide hidden Finder files
+alias show="defaults write com.apple.Finder AppleShowAllFiles -bool true && killall Finder"
+alias hide="defaults write com.apple.Finder AppleShowAllFiles -bool false && killall Finder"
+
+# hub (Github + Git https://github.com/defunkt/hub)
+alias git=hub
+
+# git
+alias gi="git init"
+alias gs="git status"
+alias ga="git add"
+alias gps="git push"
+alias gpm="git push origin master"
+alias gpl="git pull"
+alias gc="git commit"
+alias gcm="git commit -m"
+alias gl="git log"
+
+# Ruby on Rails
+alias rs="rails server"
+alias rc="rails console"
+alias rg="rails generate"
+
+# Alias commands with `bundle exec $command`
+bundle_commands="rake rspec spec cucumber cap watchr rails rackup guard thin unicorn"
+
+function run_bundler_cmd () {
+  if [ -r ./Gemfile ]; then
+    bundle exec $@
+  else
+    $@
+  fi
+}
+
+for cmd in $bundle_commands; do
+  alias $cmd="run_bundler_cmd $cmd"
+done
